9626aa133865f3ae8d3e4f5b8cfaa97f523f05c4
AMMARALBESANI <Ammarmohammed018@gmail.com>
2023-04-20 00:16:56 +0300

    adding travel images and names2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './components/home/Home';
+import data from './data/db.json';
 
 import './App.css';
 
 function App() {
   return (
   <>
-   <Home/>
+   <Home data={data}/>
   </>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,10 @@
 function Footer(){
-    return(<>
-    </>)
+    return(<footer>
+     
+          <a href="https://github.com/AMMARALBESANI"><i class="fa-brands fa-github"></i></a>
+          <p>Ammar Albesani</p>
+
+    </footer>)
 }
+
+export default Footer
